287bb7fb29363c2ab100189bd342858dc894bf49
Ivan Kocienski <ivan.kocienski@gmail.com>
2016-04-02 12:37:38 +0100

    User password validation, encryption and verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,28 @@ class User < ActiveRecord::Base
     format: { with: /\A\w+\Z/, message: 'Contains bad characters' },
     presence: true
 
+  #
+  # password stuff
+  #
+
+  attr_accessor :password
+
+  validates :password, length: { minimum: 5 }, allow_nil: false
+
+  before_save :encode_password
+
+  def test_password(string)
+    test = Digest::SHA1.hexdigest(string + self.password_salt)
+
+    test == self.password_encrypted
+  end
+
+  def encode_password
+    return if @password.nil?
+
+    self.password_salt      = Digest::SHA1.hexdigest(self.email + Date.today.to_s + "user-group-api")
+    self.password_encrypted = Digest::SHA1.hexdigest(@password + self.password_salt)
+    nil 
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,10 +18,12 @@ RSpec.describe User, type: :model do
     end
 
     it 'must be unique' do
-      u1 = User.new
-      u1.username = 'username'
-      u1.email    = 'user@example.com'
-      u1.save
+      User.new do |u|
+        u.username = 'username'
+        u.email    = 'user@example.com'
+        u.password = 'password'
+        u.save
+      end
 
       u2 = User.new
       u2.email = 'user@example.com'
@@ -35,9 +37,11 @@ RSpec.describe User, type: :model do
   context 'username' do
     it 'can be saved' do
       expect {
-        User.create( 
-                    email: 'user@example.com',
-                    username: 'user')
+        User.create do |u|
+          u.email    = 'user@example.com'
+          u.username = 'user'
+          u.password = 'password'
+        end
       }.to change( User, :count ).by(1)
 
       User.first.tap do |u|
@@ -74,6 +78,56 @@ RSpec.describe User, type: :model do
 
       err = u.errors[:username]
       expect(err).to include("Contains bad characters")
+    end
+  end
+
+  context 'password' do
+
+    def make_user( args = {} )
+      User.new do |u|
+        u.email    = args[:email] || 'user@eample.com'
+        u.username = args[:username] || 'auser'
+        u.password = args[:password] || 'passwordpassword'
+      end
+    end
+
+    context 'setting' do
+      it 'must be present' do
+        u = User.new
+        u.email    = 'user@eample.com'
+        u.username = 'auserdisplayname'
+
+        expect(u.valid?).to be_falsey
+        expect(u.errors).to have_key(:password)
+      end
+
+      it 'is salted and saved in DB' do
+        user = make_user( password: '12345', password_confirmation: '12345' )
+
+        user.save
+        user.reload
+
+        expect(user.password_encrypted.length > 0).to be(true)
+        expect(user.password_encrypted).not_to eq('12345')
+        expect(user.password_salt.length > 0).to be(true)
+      end
+    end
+
+    context 'verifying' do
+      let(:user) { make_user( password: '12345' ) }
+
+      before :each do
+        user.save
+        user.reload
+      end
+
+      it 'correct positive' do 
+        expect( user.test_password('12345') ).to be true
+      end
+
+      it 'correct negative' do 
+        expect( user.test_password('54321') ).not_to be true
+      end
     end
   end
 
