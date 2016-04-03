e0a8434823818042b2507e472ae2e3f97021f185
Ivan Kocienski <ivan.kocienski@gmail.com>
2016-04-03 23:21:51 +0100

    Users can now be linked to groups

--- app/controllers/api/v1/group_users_controller.rb
+++ app/controllers/api/v1/group_users_controller.rb
@@ -1,7 +1,31 @@
 class Api::V1::GroupUsersController < Api::V1::BaseController
 
+  before_filter :find_group
+  
   def create
+    user_params = params[:user] || {}
+    user = User.where(id: user_params[:id]).first
+
+    if user.nil?
+      return render status: 422, json: { message: 'Could not find user' }
+    end
+
+    begin
+      user.groups << @group
+
+    rescue ActiveRecord::RecordInvalid => e
+      return render status: 422, json: { message: 'Failed to link user to group', errors: e.record.errors }
+    end
+
     render json: { message: 'success' }
+  end
+
+  private
+  
+  def find_group
+    @group = Group.where(id: params[:group_id]).first
+    return if @group
+    render status: 422, json: { message: 'A group with that ID could not be found' }
   end
 
 end
--- spec/controllers/api/v1/group_users_controller_spec.rb
+++ spec/controllers/api/v1/group_users_controller_spec.rb
@@ -2,15 +2,111 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::GroupUsersController, type: :controller do
 
-  context 'create' do
-    it 'is successful' do
-      payload = {
-        group_id: 123,
-        format: :json }
+  let(:group) { Group.create(name: 'name') }
 
-      post :create, payload
-      expect(response).to be_success
+  context 'before filters' do
+    context 'find_group' do
+
+      controller do 
+        before_filter :find_group
+
+        def index
+          render text: '' 
+        end
+      end
+
+      it 'sets up @group' do
+        payload = {
+          group_id: group.id,
+          format: :json }
+
+        get :index, payload
+        expect(response).to be_success
+        expect(assigns[:group]).to eq(group)
+      end
+
+      it 'reports if group not found' do
+        payload = {
+          group_id: 1234,
+          format: :json }
+
+        get :index, payload
+        expect(response.status).to eq(422) # unprocessible entity
+        
+        data = JSON.parse(response.body)
+        expect(data['message']).to eq('A group with that ID could not be found')
+
+      end
+    end # find_group
+  end # before filters
+
+  context 'create' do
+    context 'with valid input' do
+      it 'creates link and responds sucessfully' do
+
+        user = User.create(
+          username: 'username',
+          password: 'password',
+          email:    'user@example.com')
+
+        payload = {
+          group_id: group.id,
+          user: { id: user.id },
+          format: :json }
+
+        expect {
+          post :create, payload
+        }.to change(UserGroup, :count).by(1)
+
+        expect(response).to be_success
+      end
     end
+
+    context 'with invalid input' do
+      context 'for user' do
+        it 'gets rejected' do
+          payload = {
+            group_id: group.id,
+            user: { id: 1234 },
+            format: :json }
+
+          post :create, payload 
+          expect(response.status).to eq(422) # unprocessable
+
+          data = JSON.parse(response.body)
+          expect(data['message']).to eq('Could not find user') 
+        end
+      end # for user
+
+      context 'for group' do
+        it 'gets rejected' do
+
+          user = User.create(
+            username: 'username',
+            password: 'password',
+            email:    'user@example.com')
+
+          user.groups << group
+
+          payload = {
+            group_id: group.id,
+            user: { id: user.id },
+            format: :json }
+
+          post :create, payload
+          expect(response.status).to eq(422) # unprocessable
+
+          data = JSON.parse(response.body)
+          expect(data['message']).to eq('Failed to link user to group') 
+
+          errors = {
+            "user" => ["already belongs to group"]}
+
+          expect(data['errors']).to eq(errors)
+        end
+
+      end # for group 
+    end # with invalid input
   end # create
 
 end
