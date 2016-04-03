4d7e337b0701cd4d00887d2fde5ccb56c800e8b1
Ivan Kocienski <ivan.kocienski@gmail.com>
2016-04-03 22:35:35 +0100

    Users can create groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::GroupsController < Api::V1::BaseController
 
   def create
-    render json: { group: { id: 1234 }}
+    group_params = params[:group] || {}
+
+    group = Group.new
+    group.name = group_params[:name]
+
+    if group.save 
+      render json: { group: { id: group.id }}
+    else
+      render status: 422, json: { message: 'Failed to create group', errors: group.errors }
+    end
   end
 
 end
--- spec/controllers/api/v1/groups_controller_spec.rb
+++ spec/controllers/api/v1/groups_controller_spec.rb
@@ -4,9 +4,49 @@ RSpec.describe Api::V1::GroupsController, type: :controller do
 
   context 'create' do
 
-    it 'is successful' do
-      post :create
-    end
+    context 'with valid input' do
+      let(:payload) {{
+        group: { name: 'newgroup' },
+        format: :json }}
+
+      it 'creates group' do 
+        expect {
+          post :create, payload
+        }.to change(Group, :count).by(1)
+      end
+
+      it 'returns ID' do
+        post :create, payload
+        expect(response).to be_success
+
+        data = JSON.parse(response.body)
+        group = Group.first
+        expect(data['group']['id']).to eq(group.id)
+        
+      end
+    end # with valid input
+
+    context 'with invalid data' do
+      it 'returns meaningful info to user' do
+        payload = {
+          format: :json }
+
+        post :create, payload
+        expect(response.status).to eq(422) # unprocessable
+
+        data = JSON.parse(response.body)
+        expect(data['message']).to eq('Failed to create group')
+
+        expected_errors = {
+          "name" => [
+            "is too short (minimum is 3 characters)", 
+            "Contains bad characters", 
+            "can't be blank"]}
+
+        expect(data['errors']).to eq(expected_errors)
+
+      end
+    end # with invalid data
   end
 
 end
