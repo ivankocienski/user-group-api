5858251b5f3fd79e992d18b73d3b74c51476e1cc
Ivan Kocienski <ivan.kocienski@gmail.com>
2016-04-03 17:26:43 +0100

    UserAuthToken expiry logic

--- app/models/user_auth_token.rb
+++ app/models/user_auth_token.rb
@@ -1,2 +1,9 @@
 class UserAuthToken < ActiveRecord::Base
+
+  EXPIRE_TIME = 1.hours
+
+  def expired?
+    created_at <= EXPIRE_TIME.ago
+  end 
+
 end
--- spec/models/user_auth_token_spec.rb
+++ spec/models/user_auth_token_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 RSpec.describe UserAuthToken, type: :model do
 
+  context '#expired?' do
+    it 'is false if record is new' do
+      at = UserAuthToken.create
+      expect(at.expired?).to be_falsey
+    end
+
+    it 'is true if record is too old' do
+      at = UserAuthToken.create(created_at: (UserAuthToken::EXPIRE_TIME + 10).hours.ago)
+      expect(at.expired?).to be_truthy
+    end
+  end
 
 end
